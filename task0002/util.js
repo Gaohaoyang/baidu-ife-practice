1cb789696be58d0cc368512033118b6c313a3710
gaohaoyang126@126.com <gaohaoyang126@126.com>
2015-04-24 21:42:06 +0800

    new

--- task0002/util.js
+++ task0002/util.js
@@ -1,9 +1,49 @@
 // 判断arr是否为一个数组，返回一个bool值
 function isArray(arr) {
-    // your implement
+    return typeof arr === "object" && Object.prototype.toString.call(arr) === "[object Array]";
 }
 
 // 判断fn是否为一个函数，返回一个bool值
 function isFunction(fn) {
+    return typeof fn === "function";
+}
+
+// 使用递归来实现一个深度克隆，可以复制一个目标对象，返回一个完整拷贝
+// 被复制的对象类型会被限制为数字、字符串、布尔、日期、数组、Object对象。不会包含函数、正则对象等
+function cloneObject(src) {
     // your implement
 }
+
+// 测试用例：
+var srcObj = {
+    a: 1,
+    b: {
+        b1: ["hello", "hi"],
+        b2: "JavaScript"
+    }
+};
+var abObj = srcObj;
+var tarObj = cloneObject(srcObj);
+
+srcObj.a = 2;
+srcObj.b.b1[0] = "Hello";
+
+console.log(abObj.a);
+console.log(abObj.b.b1[0]);
+
+console.log(tarObj.a);      // 1
+console.log(tarObj.b.b1[0]);    // "hello"
+
+/**
+ * test
+ */
+function a() {
+
+}
+var b = "abc";
+var c = [];
+console.log("isFunction----->" + isFunction(a));
+console.log("isArray-------->"+isArray(c));
+
+Array.isArray([]);
+Array.isArray({});
