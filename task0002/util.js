00c3bc9f732f0b75e1bf68c16ff53e61ed9bcb43
gaohaoyang126@126.com <gaohaoyang126@126.com>
2015-04-25 21:50:23 +0800

    deep clone

--- task0002/util.js
+++ task0002/util.js
@@ -1,3 +1,6 @@
+/**
+ * Gaohaoyang
+ */
 // 判断arr是否为一个数组，返回一个bool值
 function isArray(arr) {
     return typeof arr === "object" && Object.prototype.toString.call(arr) === "[object Array]";
@@ -8,10 +11,26 @@ function isFunction(fn) {
     return typeof fn === "function";
 }
 
+/**
+ * test
+ */
+function a() {}
+var b = "abc";
+var c = [];
+console.log("isFunction----->" + isFunction(a));
+console.log("isArray-------->" + isArray(c));
+
+Array.isArray([]);
+Array.isArray({});
+
 // 使用递归来实现一个深度克隆，可以复制一个目标对象，返回一个完整拷贝
 // 被复制的对象类型会被限制为数字、字符串、布尔、日期、数组、Object对象。不会包含函数、正则对象等
 function cloneObject(src) {
     // your implement
+    var resultObj = {};
+    for(var i in src){  //遍历这个对象
+
+    }
 }
 
 // 测试用例：
@@ -20,6 +39,9 @@ var srcObj = {
     b: {
         b1: ["hello", "hi"],
         b2: "JavaScript"
+    },
+    c: function() {
+        console.log("function");
     }
 };
 var abObj = srcObj;
@@ -31,19 +53,35 @@ srcObj.b.b1[0] = "Hello";
 console.log(abObj.a);
 console.log(abObj.b.b1[0]);
 
-console.log(tarObj.a);      // 1
-console.log(tarObj.b.b1[0]);    // "hello"
+// console.log(tarObj.a);      // 1
+// console.log(tarObj.b.b1[0]);    // "hello"
 
-/**
- * test
- */
-function a() {
+console.log(srcObj.b);
 
+function showProps(obj, objName) {
+    var result = "";
+    for (var i in obj) {
+        if (obj.hasOwnProperty(i)) {
+            result += objName + "." + i + " = " + obj[i] + "\n";
+        }
+    }
+    return result;
 }
-var b = "abc";
-var c = [];
-console.log("isFunction----->" + isFunction(a));
-console.log("isArray-------->"+isArray(c));
 
-Array.isArray([]);
-Array.isArray({});
+function showPropsWithoutFun(obj, objName) {
+    var result = "";
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) {       //跳过继承属性
+            continue;
+        }
+        if (typeof obj[i] === "function") { //跳过这个对象的方法
+            continue;
+        }
+        result += objName + "." + i + "=" + obj[i] + "\n";
+    }
+    return result;
+}
+
+console.log(showProps(srcObj, 'srcObj'));
+console.log(showPropsWithoutFun(srcObj, 'srcObj'));
+// console.log('prototype'+srcObj.prototype.toString.call(Object));    
