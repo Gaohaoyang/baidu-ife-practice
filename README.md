4bc53d163a5f956662babd67a509f764ff6dec30
gaohaoyang126@126.com <gaohaoyang126@126.com>
2015-04-26 18:51:14 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# 百度Web前端技术学院
+
+## 学院介绍
+
+**百度Web前端技术学院**（Baidu Institute of Front-End Technology简称IFE）是一个由百度EFE团队、百度人力资源部校园招聘组联合出品的、面向在校大学生的前端培训组织，我们希望能够借助百度大量优秀的前端工程师以及丰富的前端知识积累，帮助大学生们更加高效、系统地学习Web前端技术。
+
+## 学院主页
+
+[https://github.com/baidu-ife/ife](https://github.com/baidu-ife/ife)
+
+## About
+
+这个仓库用来存放我在百度Web前端技术学院学习期间做的所有任务。
--- task0002/util.js
+++ task0002/util.js
@@ -11,17 +11,6 @@ function isFunction(fn) {
     return typeof fn === "function";
 }
 
-/**
- * test
- */
-function a() {}
-var b = "abc";
-var c = [];
-console.log("isFunction----->" + isFunction(a));
-console.log("isArray-------->" + isArray(c));
-
-Array.isArray([]);
-Array.isArray({});
 
 // 使用递归来实现一个深度克隆，可以复制一个目标对象，返回一个完整拷贝
 // 被复制的对象类型会被限制为数字、字符串、布尔、日期、数组、Object对象。不会包含函数、正则对象等
@@ -45,55 +34,73 @@ function cloneObject(src) {
     return o;
 }
 
-// 测试用例：
-var srcObj = {
+// 对数组进行去重操作，只考虑数组中元素为数字或字符串，返回一个去重后的数组
+function uniqArray(arr) {
+    var newArr = [];    //创建空数组
+    for (var i in arr) {    //遍历旧数组
+        if (newArr.indexOf(arr[i]) == -1) {//如果新数组中不存在当前元素
+            newArr.push(arr[i]);//新数组中加入当前元素
+        }
+    }
+    return newArr;
+}
+
+// 中级班同学跳过此题
+// 实现一个简单的trim函数，用于去除一个字符串，头部和尾部的空白字符
+// 假定空白字符只有半角空格、Tab
+// 练习通过循环，以及字符串的一些基本方法，分别扫描字符串str头部和尾部是否有连续的空白字符，并且删掉他们，最后返回一个完成去除的字符串
+function simpleTrim(str) {
+    for(var i = 0;i<str.length;i++){
+        if(str.charAt(i)===" "||str.charAt(i)=="\t"){
+            
+        }
+    }
+    
+}
+
+// 很多同学肯定对于上面的代码看不下去，接下来，我们真正实现一个trim
+// 对字符串头尾进行空格字符的去除、包括全角半角空格、Tab等，返回一个字符串
+// 尝试使用一行简洁的正则表达式完成该题目
+function trim(str) {
+    // your implement
+}
+
+// 使用示例
+var str = '   hi!  ';
+str = trim(str);
+console.log(str); // 'hi!'
+
+// 实现一个遍历数组的方法，针对数组中每一个元素执行fn函数，并将数组索引和元素作为参数传递
+function each(arr, fn) {
+    // your implement
+}
+
+// 其中fn函数可以接受两个参数：item和index
+
+// 使用示例
+var arr = ['java', 'c', 'php', 'html'];
+function output(item) {
+    console.log(item);
+}
+each(arr, output);  // java, c, php, html
+
+// 使用示例
+var arr = ['java', 'c', 'php', 'html'];
+function output(item, index) {
+    console.log(index + ': ' + item);
+}
+each(arr, output);  // 0:java, 1:c, 2:php, 3:html
+
+// 获取一个对象里面第一层元素的数量，返回一个整数
+function getObjectLength(obj) {}
+
+// 使用示例
+var obj = {
     a: 1,
-    b: {
-        b1: ["hello", "hi"],
-        b2: "JavaScript"
-    },
-    c: function() {
-        console.log("function");
+    b: 2,
+    c: {
+        c1: 3,
+        c2: 4
     }
 };
-var abObj = srcObj;
-var tarObj = cloneObject(srcObj);
-
-srcObj.a = 2;
-srcObj.b.b1[0] = "Hello";
-
-console.log(abObj.a);
-console.log(abObj.b.b1[0]);
-
-console.log(tarObj.a); // 1
-console.log(tarObj.b.b1[0]); // "hello"
-
-console.log(srcObj.b);
-
-function showProps(obj, objName) {
-    var result = "";
-    for (var i in obj) {
-        if (obj.hasOwnProperty(i)) {
-            result += objName + "." + i + " = " + obj[i] + "\n";
-        }
-    }
-    return result;
-}
-
-function showPropsWithoutFun(obj, objName) {
-    var result = "";
-    for (var i in obj) {
-        if (!obj.hasOwnProperty(i)) { //跳过继承属性
-            continue;
-        }
-        if (typeof obj[i] === "function") { //跳过这个对象的方法
-            continue;
-        }
-        result += objName + "." + i + "=" + obj[i] + "\n";
-    }
-    return result;
-}
-
-console.log(showProps(srcObj, 'srcObj'));
-console.log(showPropsWithoutFun(srcObj, 'srcObj'));
-// console.log('prototype'+srcObj.prototype.toString.call(Object));
+console.log(getObjectLength(obj)); // 3
