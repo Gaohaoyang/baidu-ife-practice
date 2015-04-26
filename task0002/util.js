e1cc04510cf8312db3b6e7900f21c5a5f550646f
gaohaoyang126@126.com <gaohaoyang126@126.com>
2015-04-26 22:14:09 +0800

    RegExp

--- task0002/util.js
+++ task0002/util.js
@@ -34,65 +34,107 @@ function cloneObject(src) {
     return o;
 }
 
+// 测试用例：
+var srcObj = {
+    a: 1,
+    b: {
+        b1: ["hello", "hi"],
+        b2: "JavaScript"
+    }
+};
+var abObj = srcObj;
+var tarObj = cloneObject(srcObj);
+
+srcObj.a = 2;
+srcObj.b.b1[0] = "Hello";
+
+console.log(abObj.a);
+console.log(abObj.b.b1[0]);
+
+console.log(tarObj.a); // 1
+console.log(tarObj.b.b1[0]); // "hello"
+
+
 // 对数组进行去重操作，只考虑数组中元素为数字或字符串，返回一个去重后的数组
 function uniqArray(arr) {
-    var newArr = [];    //创建空数组
-    for (var i in arr) {    //遍历旧数组
-        if (newArr.indexOf(arr[i]) == -1) {//如果新数组中不存在当前元素
-            newArr.push(arr[i]);//新数组中加入当前元素
+    var newArr = []; //创建空数组
+    for (var i in arr) { //遍历旧数组
+        if (newArr.indexOf(arr[i]) == -1) { //如果新数组中不存在当前元素
+            newArr.push(arr[i]); //新数组中加入当前元素
         }
     }
     return newArr;
 }
+// 使用示例
+var a = [1, 3, 5, 7, 5, 3];
+var b = uniqArray(a);
+console.log(b); // [1, 3, 5, 7]
+
+
 
 // 中级班同学跳过此题
 // 实现一个简单的trim函数，用于去除一个字符串，头部和尾部的空白字符
 // 假定空白字符只有半角空格、Tab
 // 练习通过循环，以及字符串的一些基本方法，分别扫描字符串str头部和尾部是否有连续的空白字符，并且删掉他们，最后返回一个完成去除的字符串
 function simpleTrim(str) {
-    for(var i = 0;i<str.length;i++){
-        if(str.charAt(i)===" "||str.charAt(i)=="\t"){
-            
+    var i;
+    var j;
+    for (i = 0; i < str.length; i++) { //从头遍历字符串
+        if (str.charAt(i) != " " && str.charAt(i) != "\t") { //当不为空的时候
+            break; //跳出循环
         }
     }
-    
+    for (j = str.length - 1; j >= 0; j--) {
+        if (str.charAt(j) != " " && str.charAt(j) != "\t") { //当不为空的时候
+            break; //跳出循环
+        }
+    }
+    return str.slice(i, j + 1); //返回子字符串
 }
 
 // 很多同学肯定对于上面的代码看不下去，接下来，我们真正实现一个trim
 // 对字符串头尾进行空格字符的去除、包括全角半角空格、Tab等，返回一个字符串
 // 尝试使用一行简洁的正则表达式完成该题目
 function trim(str) {
-    // your implement
+    return str.replace(/^\s+|\s+$/g, '');
+    //匹配开头和结尾的空白字符，并全局匹配
 }
 
 // 使用示例
-var str = '   hi!  ';
+var str = '   hi! World!  ';
+console.log("simpleTrim--->" + simpleTrim(str));
 str = trim(str);
-console.log(str); // 'hi!'
+console.log("trim--->" + str); // 'hi!'
 
 // 实现一个遍历数组的方法，针对数组中每一个元素执行fn函数，并将数组索引和元素作为参数传递
 function each(arr, fn) {
-    // your implement
+    for (var i in arr) {
+        fn(arr[i], i);
+    }
 }
 
 // 其中fn函数可以接受两个参数：item和index
 
 // 使用示例
 var arr = ['java', 'c', 'php', 'html'];
-function output(item) {
+
+function output1(item) {
     console.log(item);
 }
-each(arr, output);  // java, c, php, html
+each(arr, output1); // java, c, php, html
 
 // 使用示例
 var arr = ['java', 'c', 'php', 'html'];
+
 function output(item, index) {
     console.log(index + ': ' + item);
 }
-each(arr, output);  // 0:java, 1:c, 2:php, 3:html
+each(arr, output); // 0:java, 1:c, 2:php, 3:html
 
 // 获取一个对象里面第一层元素的数量，返回一个整数
-function getObjectLength(obj) {}
+function getObjectLength(obj) {
+    return Object.keys(obj).length;
+}
 
 // 使用示例
 var obj = {
@@ -104,3 +146,13 @@ var obj = {
     }
 };
 console.log(getObjectLength(obj)); // 3
+
+// 判断是否为邮箱地址
+function isEmail(emailStr) {
+    // your implement
+}
+
+// 判断是否为手机号
+function isMobilePhone(phone) {
+    // your implement
+}
